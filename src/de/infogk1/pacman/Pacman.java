5b41cbc72e79359b2b68673da0d63b53a09ba161
dittich <sv@dittich.de>
2017-05-12 18:19:50 +0200

    Pacman als JPanel Dienste auf Panel in Pacman importiert texture als BufferedImage - Wird später durch Animation ersetzt.

--- src/de/infogk1/pacman/Pacman.java
+++ src/de/infogk1/pacman/Pacman.java
@@ -1,19 +1,21 @@
 package de.infogk1.pacman;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageOp;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
-public class Pacman {
+public class Pacman extends JPanel{
 
 	float x;
 	float y;
 	int speed = 500;
+	long lastFrame = System.currentTimeMillis();
 	BufferedImage texture;
 	
 	public Pacman(int x, int y){
@@ -24,16 +26,19 @@ public class Pacman {
 	public void draw(Graphics2D g){
 		g.drawString("Das ist Pacman", 40, 40);
 		g.setColor(Color.YELLOW);
+		
 		try {
-			texture = ImageIO.read(getClass().getClassLoader().getResourceAsStream("res\\pacman.jpg"));
+			texture = ImageIO.read(new File("res/pacman.jpg"));
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//g.fillOval((int)x, (int)y, 50, 50);
+
 		g.drawImage(texture, null, (int)x, (int)y);
 	}
 	
 	public void update(float tslf){
+		
 		if(Listener.keys.get("right").isLastKey())
 			x += speed*tslf;
 		if(Listener.keys.get("left").isLastKey())
@@ -42,6 +47,22 @@ public class Pacman {
 			y -= speed*tslf;
 		if(Listener.keys.get("down").isLastKey())
 			y += speed*tslf;
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		super.paintComponent(g2);
+		this.draw(g2);
+		update();
+	}
+	
+	
+	public void update(){
+		long thisFrame = System.currentTimeMillis();
+		float tslf = (thisFrame-lastFrame)/1000f;
+		lastFrame = thisFrame;
+		this.update(tslf);
 	}
 	
 }
