fbf302934b318e6a05104d2c1d0729b0a0e58575
carst <carst@DESKTOP-5JVIH3I.fritz.box>
2017-06-09 20:43:01 +0200

    Maze initialisiert Pacman läuft auf Maze Ghost initialisiert und animiert

---
+++ src/de/dittich/pacman/Maze.java
@@ -0,0 +1,39 @@
+package de.dittich.pacman;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Maze {
+
+	public static void loadMaze() {
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(new FileInputStream(new File(Var.fileMaze))));
+			for (int i = 0; i < 800; i++) {
+				String line = br.readLine();
+				char[] c = line.toCharArray();
+				for (int j = 0; j < 1000; j++) {
+					Var.maze[j][i] = Integer.parseInt("" + c[j]);
+					if (Integer.parseInt("" + c[j]) == 2) {
+						Var.xPac = j;
+						Var.yPac = i;
+					}
+					else if(Integer.parseInt("" + c[j]) == 3) {
+						Var.xG1 = j;
+						Var.yG1 = i;
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
